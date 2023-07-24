acecfd2a453726e6b4b6d210fdf6be569ca6b65c
Redouanesad <redouanesadouk7@gmail.com>
2023-07-25 00:24:51 +0100

    Updated

--- handle_print.c
+++ handle_print.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the argument.
@@ -10,10 +11,11 @@
  * @precision: Precision specification
  * @size: Size specifier
  *
- * Return 1 or 2;
- */
+ * Return: 1 or 2;
+*/
+
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-	int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] =  {
